396090b5b3dd631f1ee8dfd7b83834ffc2525444
KornelKovats <kovats.kornel95@gmail.com>
2020-10-29 09:40:14 +0100

    not working login, but not throw error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/Login/Login.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Router exact path="/login" component={Login} />
     </div>
   );
 }
