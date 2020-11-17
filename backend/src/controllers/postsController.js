5fac308525e025f6e25329b66b6cebb4f7231ece
KornelKovats <kovats.kornel95@gmail.com>
2020-11-17 21:55:42 +0100

    postService added

--- backend/src/controllers/postsController.js
+++ backend/src/controllers/postsController.js
@@ -1,16 +1,12 @@
-import { helloService } from '../services';
-import  postsSchema  from '../models/posts';
 import mongoose from 'mongoose';
+import postsSchema from '../models/posts';
 
-export const helloController = {
+export const postsController = {
   async get(req, res, next) {
     try {
-      console.log(postsSchema);
-      
       console.log(mongoose.connection.readyState);
       let asd = await postsSchema.create({title: "New COVID Vacine", author: "Robbert mcDonald"});
       const data = await postsSchema.find({title: "New COVID Vacine"});
-      //let data = helloService.getHelloWorld();
       res.status(200).json(data);
     } catch (error) {
         next(error);
